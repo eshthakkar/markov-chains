d8fe59120424de91d0bdf8b741d75a7aa677dfb6
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-05 15:06:44 -0700

    Revert "n-gram function not fully working"
    This reverts commit 8530b4766ec196827f23b5f0d50c37883df1adc7.

--- markov.py
+++ markov.py
@@ -17,7 +17,7 @@ def open_and_read_file(file_path1,file_path2=""):
     return contents
 
 
-def make_chains(text_string,n):
+def make_chains(text_string):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -34,60 +34,34 @@ def make_chains(text_string,n):
 
     # your code goes here
     words = text_string.split()
-    for i in range(len(words) - n):
-        words_list = []
-
-        for j in range(n):
-            words_list.append(words[i+j])
-        words_keys = tuple(words_list)
-        #print words_keys  #Used for debugging
-        
-
-        if words_keys not in chains:
-            chains[words_keys] = []
-        chains[words_keys].append(words[i + n])
-    print chains  # Used for debugging    
+    for i in range(len(words) - 2):
+        words_pairs = (words[i], words[i + 1])
+        if words_pairs not in chains:
+            chains[words_pairs] = []
+        chains[words_pairs].append(words[i + 2])
     return chains
 
 
-def make_text(chains,n):
+def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
     text = ""
-    new_list_key = []
-    next_list_key = []
 
 
     # your code goes here
     while(True):
         link = choice(chains.keys())   # First key at random
         if link[0][0].isupper():
-            for i in range(len(link)):
-                text += link[i] + " " # Add that key in the text
+            text += link[0] + " " + link[1] # Add that key in the text
             break 
-    #print text 
 
-    # Creating a new key tuple
-    for i in range(1,n): 
-        new_list_key.append(link[i])
-    new_list_key.append(choice(chains[link]))
-    new_link = tuple(new_list_key)  # create the new key
-
+    new_link = (link[1], choice(chains[link]))  # create the new key
     while(new_link in chains):
-        for i in range(len(new_link)):
-            text += new_link[i] + " " # Add that key in the text
-
-
-        for i in range(1,n): 
-            next_list_key.append(new_link[i])
-        next_key_tup = tuple(next_list_key)
-        next_link = (next_key_tup, choice(chains[new_link]))  # create the new key
-
+        text += " " + new_link[1]
+        next_link = (new_link[1], choice(chains[new_link]))  # create the new key
         new_link = next_link
 
-    #for i in range(len(new_link)):
-    #    text += new_link[i] + " " # Add that key in the text
-    #text += " " + new_link[1]        
+    text += " " + new_link[1]        
 
     return text
 
@@ -102,9 +76,9 @@ else:
     input_text = open_and_read_file(input_path1,argv[2])    
 
 # Get a Markov chain
-chains = make_chains(input_text,2)
+chains = make_chains(input_text)
 
 # Produce random text
-random_text = make_text(chains,2)
+random_text = make_text(chains)
 
 print random_text
